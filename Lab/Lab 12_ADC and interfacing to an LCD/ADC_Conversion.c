f7664c0565150094cbaf22bacb0c095e22510549
Janice Bargoria <jerotichjanice@gmail.com>
2026-04-30 19:29:53 -0700

    Add files via upload

---
+++ Lab/Lab 12_ADC and interfacing to an LCD/ADC_Conversion.c
@@ -0,0 +1,144 @@
+/*
+ * The purpose of this program is demonstrate How ADC operates. 
+ * Conenct your input to RA0. 
+ * Complete the code by modifying all the places identified by "DO:"
+ * Use a port to represent the input voltage in binary.  
+ * Author: Farid Farahmand
+ */
+
+
+// PIC18F46K42 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1L
+#pragma config FEXTOSC = LP     // External Oscillator Selection (LP (crystal oscillator) optimized for 32.768 kHz; PFM set to low power)
+#pragma config RSTOSC = EXTOSC  // Reset Oscillator Selection (EXTOSC operating per FEXTOSC bits (device manufacturing default))
+
+// CONFIG1H
+#pragma config CLKOUTEN = OFF   // Clock out Enable bit (CLKOUT function is disabled)
+#pragma config PR1WAY = ON      // PRLOCKED One-Way Set Enable bit (PRLOCK bit can be cleared and set only once)
+#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+
+// CONFIG2L
+#pragma config MCLRE = EXTMCLR  // MCLR Enable bit (If LVP = 0, MCLR pin is MCLR; If LVP = 1, RE3 pin function is MCLR )
+#pragma config PWRTS = PWRT_OFF // Power-up timer selection bits (PWRT is disabled)
+#pragma config MVECEN = ON      // Multi-vector enable bit (Multi-vector enabled, Vector table used for interrupts)
+#pragma config IVT1WAY = ON     // IVTLOCK bit One-way set enable bit (IVTLOCK bit can be cleared and set only once)
+#pragma config LPBOREN = OFF    // Low Power BOR Enable bit (ULPBOR disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled , SBOREN bit is ignored)
+
+// CONFIG2H
+#pragma config BORV = VBOR_2P45 // Brown-out Reset Voltage Selection bits (Brown-out Reset Voltage (VBOR) set to 2.45V)
+#pragma config ZCD = OFF        // ZCD Disable bit (ZCD disabled. ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
+#pragma config PPS1WAY = ON     // PPSLOCK bit One-Way Set Enable bit (PPSLOCK bit can be cleared and set only once; PPS registers remain locked after one clear/set cycle)
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config DEBUG = OFF      // Debugger Enable bit (Background debugger disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Extended Instruction Set and Indexed Addressing Mode disabled)
+
+// CONFIG3L
+#pragma config WDTCPS = WDTCPS_31// WDT Period selection bits (Divider ratio 1:65536; software control of WDTPS)
+#pragma config WDTE = OFF       // WDT operating mode (WDT Disabled; SWDTEN is ignored)
+
+// CONFIG3H
+#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
+#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
+
+// CONFIG4L
+#pragma config BBSIZE = BBSIZE_512// Boot Block Size selection bits (Boot Block size is 512 words)
+#pragma config BBEN = OFF       // Boot Block enable bit (Boot block disabled)
+#pragma config SAFEN = OFF      // Storage Area Flash enable bit (SAF disabled)
+#pragma config WRTAPP = OFF     // Application Block write protection bit (Application Block not write protected)
+
+// CONFIG4H
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block not write-protected)
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+#pragma config WRTSAF = OFF     // SAF Write protection bit (SAF not Write Protected)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored)
+
+// CONFIG5L
+#pragma config CP = OFF         // PFM and Data EEPROM Code Protection bit (PFM and Data EEPROM code protection disabled)
+
+#include <xc.h> // must have this
+//#include "../../../../../Program Files/Microchip/xc8/v2.40/pic/include/proc/pic18f46k42.h"
+//#include "C:\Program Files\Microchip\xc8\v2.40\pic\include\proc\pic18f46k42"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#define _XTAL_FREQ 4000000                 // Fosc  frequency for _delay()  library
+#define FCY    _XTAL_FREQ/4
+
+#define Vref 3.3 // voltage reference 
+int digital; // holds the digital value 
+float voltage; // hold the analog value (volt))
+char data[12];
+
+void ADC_Init(void) // DO: Declare void ADC_Init
+{
+    TRISD = 0; //RD As Output
+    LATD = 0;
+    ANSELD = 0x00;
+    
+    TRISB = 0; //RD As Output
+    LATB = 0;
+    ANSELB = 0x00;
+    
+    TRISAbits.TRISA0 = 1;  //Set RA0 to input
+    ANSELAbits.ANSELA0 =1;  //Set RA0 to analog
+    
+    ADCON0bits.FM =1; // set right justify
+    ADCON0bits.CS =1; //set ADCRC Clock
+    
+    ADCLK = 0x00;  //set ADC CLOCK Selection register to zero
+    ADPCH = 0x00; //Set RA0 as Analog channel in ADC ADPCH
+    
+    ADCAP = 0x00; //
+    ADREF = 0x00;
+    
+    ADPREL = 0x00;  //set pre-charge select to 0 in register ADPERL & ADPERH
+    ADPREH = 0x00;
+    
+    ADACQL = 0x00; //acquisition Charge share time
+    ADACQH = 0x00;
+    
+    ADPCH = 0x00;
+    ADCON0bits.ON = 1; //DO: Turn ADC On on register ADCON0
+    
+    //Clear ADCIF; //clear ADC
+}
+
+/*This code block configures the ADC
+for polling, VDD and VSS references, ADCRC
+oscillator and AN0 input.
+Conversion start & polling for completion
+are included.
+ */
+void main() {
+    ADC_Init(); //ADC Initialization
+    //DO: CALL ADC_Init function defined below;
+    while (1) {
+        ADCON0bits.GO =1; //DO: Set ADCON0 Go to start conversion
+        while (ADCON0bits.GO); //Wait for conversion done
+        
+        digital = (ADRESH<<8) | (ADRESL);/*Combine 8-bit LSB and 2-bit MSB*/
+        voltage = digital*((float)Vref/(float)(4096));// DO: define voltage = Vref/4096 (note that voltage is float type
+         
+        
+         LATD =(digital>>5); 
+         //LATB =(digital && 0x1F);
+         LATB = (ADRESH & 0x0F);
+         
+         // DO: Write a code to translate the values from ADRESH:ADRESL register 
+        //         pair to IO Port. In this case we can connect ADRESL to Port D
+
+        /*This is used to convert integer value to ASCII string*/
+        sprintf(data,"%.2f",voltage);
+        strcat(data," V");	/*Concatenate result and unit to print*/
+    }
+}
+
+

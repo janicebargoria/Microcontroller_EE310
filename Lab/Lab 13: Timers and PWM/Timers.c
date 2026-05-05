17d72e6885e8f0d90c8d7cfd8589017a6570e712
Janice Bargoria <jerotichjanice@gmail.com>
2026-05-05 15:52:38 -0700

    Add files via upload

---
+++ Lab/Lab 13: Timers and PWM/Timers.c
@@ -0,0 +1,185 @@
+/*
+* This program configures the PIC18 to use Timer0 with interrupts to generate a 
+* periodic timing signal. The timer is driven by the system clock (FOSC/4) 
+* with a prescaler, creating an interrupt approximately every 1.3 ms. 
+* Each time the interrupt occurs, the program updates a counter and toggles 
+* an output (such as an LED), producing a visible blinking effect.
+* In short, the program demonstrates how to:
+*         Use the internal/system clock as a timing source
+*         Configure Timer0 in 8-bit mode with a prescaler
+*         Generate periodic interrupts
+*         Use those interrupts to control output behavior (e.g., LED blinking)
+*
+* Author: Janice Bargoria
+ * Date: 05/05/2026
+*/
+// PIC18F46K42 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1L
+#pragma config FEXTOSC = LP     // External Oscillator Selection (LP (crystal oscillator) optimized for 32.768 kHz; PFM set to low power)
+#pragma config RSTOSC = EXTOSC  // Reset Oscillator Selection (EXTOSC operating per FEXTOSC bits (device manufacturing default))
+
+// CONFIG1H
+#pragma config CLKOUTEN = OFF   // Clock out Enable bit (CLKOUT function is disabled)
+#pragma config PR1WAY = ON      // PRLOCKED One-Way Set Enable bit (PRLOCK bit can be cleared and set only once)
+#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+
+// CONFIG2L
+#pragma config MCLRE = EXTMCLR  // MCLR Enable bit (If LVP = 0, MCLR pin is MCLR; If LVP = 1, RE3 pin function is MCLR )
+#pragma config PWRTS = PWRT_OFF // Power-up timer selection bits (PWRT is disabled)
+#pragma config MVECEN = ON      // Multi-vector enable bit (Multi-vector enabled, Vector table used for interrupts)
+#pragma config IVT1WAY = ON     // IVTLOCK bit One-way set enable bit (IVTLOCK bit can be cleared and set only once)
+#pragma config LPBOREN = OFF    // Low Power BOR Enable bit (ULPBOR disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled , SBOREN bit is ignored)
+
+// CONFIG2H
+#pragma config BORV = VBOR_2P45 // Brown-out Reset Voltage Selection bits (Brown-out Reset Voltage (VBOR) set to 2.45V)
+#pragma config ZCD = OFF        // ZCD Disable bit (ZCD disabled. ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
+#pragma config PPS1WAY = ON     // PPSLOCK bit One-Way Set Enable bit (PPSLOCK bit can be cleared and set only once; PPS registers remain locked after one clear/set cycle)
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config DEBUG = OFF      // Debugger Enable bit (Background debugger disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Extended Instruction Set and Indexed Addressing Mode disabled)
+
+// CONFIG3L
+#pragma config WDTCPS = WDTCPS_31// WDT Period selection bits (Divider ratio 1:65536; software control of WDTPS)
+#pragma config WDTE = OFF       // WDT operating mode (WDT Disabled; SWDTEN is ignored)
+
+// CONFIG3H
+#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
+#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
+
+// CONFIG4L
+#pragma config BBSIZE = BBSIZE_512// Boot Block Size selection bits (Boot Block size is 512 words)
+#pragma config BBEN = OFF       // Boot Block enable bit (Boot block disabled)
+#pragma config SAFEN = OFF      // Storage Area Flash enable bit (SAF disabled)
+#pragma config WRTAPP = OFF     // Application Block write protection bit (Application Block not write protected)
+
+// CONFIG4H
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block not write-protected)
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+#pragma config WRTSAF = OFF     // SAF Write protection bit (SAF not Write Protected)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored)
+
+// CONFIG5L
+#pragma config CP = OFF         // PFM and Data EEPROM Code Protection bit (PFM and Data EEPROM code protection disabled)
+
+#include <xc.h> // must have this
+// You may need this - change the path
+//#include "../../../../../Program Files/Microchip/xc8/v2.40/pic/include/proc/pic18f46k42.h"
+//#include "C:\Program Files\Microchip\xc8\v2.40\pic\include\proc\pic18f46k42"
+
+#define _XTAL_FREQ 4000000                 // Fosc  frequency for _delay()  library
+#define FCY    _XTAL_FREQ/4
+
+uint8_t checkTimerValue;
+#define myLED  PORTDbits.RD1
+
+
+void (*TMR0_InterruptHandler)(void);
+
+void TMR0_Initialize(void)
+{
+
+    T0CON1 = 0x48; // Set T0CS FOSC/4; T0CKPS 1:256; T0ASYNC synchronised; 
+    
+    //TMR0H = 0x4; // Set TMR0H ;  The total division will be (FOSC/4)/(4+1)x256)))
+    TMR0H = 0x28; 
+
+    // TMR0L 0; 
+    TMR0L = 0x00;
+
+    // Make sure you Clear Interrupt flag before enabling the interrupt
+    PIR3bits.TMR0IF = 0;
+
+    // Enabling TMR0 interrupt.
+    PIE3bits.TMR0IE = 1;
+
+    // T0OUTPS 1:1; T0EN enabled; T016BIT 8-bit; 
+    T0CON0 = 0X80;
+}
+
+void TMR0_StartTimer(void)
+{
+    // Start the Timer by writing to TMR0ON bit
+    T0CON0bits.T0EN = 1;
+}
+
+void TMR0_StopTimer(void)
+{
+    // Stop the Timer by writing to TMR0ON bit
+    T0CON0bits.T0EN = 0;
+}
+
+uint8_t TMR0_ReadTimer(void)
+{
+    uint8_t readVal;
+
+    // read Timer0, low register only
+    readVal = TMR0L;
+
+    return readVal;
+}
+
+void TMR0_WriteTimer(uint8_t timerVal)
+{
+    // Write to Timer0 registers, low register only
+    TMR0L = timerVal;
+ }
+
+void TMR0_Reload(uint8_t periodVal)
+{
+   // Write to Timer0 registers, high register only
+   TMR0H = periodVal;
+}
+
+void __interrupt(irq(IRQ_TMR0), base(0x4008)) TMR0_ISR(void)
+{
+    T0CON0=0x00; // stop the timer & do what you have to do - clear it! 
+    TMR0L=0; // reset the timer
+    PIR3bits.TMR0IF = 0; // Clear the interrupt flag
+    myLED ^= 1; // ISR code goes here
+    T0CON0 = 0X80; //start the timer0
+}
+
+void __interrupt(irq(default), base(0x4008)) DEFAULT_ISR(void)
+{
+    // Unhandled interrupts go here - leave this alone
+}
+void INTERRUPT_Initialize (void)
+{
+   INTCON0bits.GIEH = 1; // Enable high priority interrupts
+   INTCON0bits.GIEL = 1; // Enable low priority interrupts
+   INTCON0bits.IPEN = 1; // Enable interrupt priority
+    
+    PIE3bits.TMR0IE = 1; // Enable TMR0 interrupt
+    PIE4bits.TMR1IE = 1;               // Enable TMR1 interrupt
+    
+    IPR3bits.TMR0IP = 0; // Make TMR0 interrupt low priority
+    
+    // Change IVTBASE if required
+    IVTBASEU = 0x00; // Optional
+    IVTBASEH = 0x40; // Default is 0x0008
+    IVTBASEL = 0x08;
+}
+
+void main (void) {
+    ANSELD = 0b00000000;    
+    TRISD= 0b00000000;//sets PORTB as all outputs 
+    PORTD= 0b00000000;//turns off PORTB outputs so that the LED is initially off
+    INTERRUPT_Initialize();
+    TMR0_Initialize();
+    TMR0_StartTimer();
+
+    while (1) {
+        PORTDbits.RD0=0;//turns on LED
+        __delay_ms(1);//delays for 1 second
+        PORTDbits.RD0=1;//turns on LED    
+        __delay_ms(1);//delays for 1 second
+    }
+    
+    
+}
